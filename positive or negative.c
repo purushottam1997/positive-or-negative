c677fde5aeb5b1efba970b1121656ff43806fb19
purushottam1997 <45572099+purushottam1997@users.noreply.github.com>
2019-06-11 15:13:19 +0530

    Create positive or negative.c

---
+++ positive or negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf(" \n");
+    scanf("%d",&number);
+
+    // True if remainder is 0
+    if( number%2 == 0 )
+        printf("even",number);
+    else
+        printf("odd",number);
+    return 0;
+}
+© 2019 GitHub, Inc.
